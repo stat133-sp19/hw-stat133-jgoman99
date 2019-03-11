340306a24fb05dca6a657842f882a3e39111e100
John Friedman <jgfriedman99@gmail.com>
2019-03-11 15:33:04 -0700

    added 90% of workout01

---
+++ workout1/code/make-shot-charts-script.R
@@ -0,0 +1,74 @@
+library("ggplot2")
+library("jpeg")
+library("grid")
+library("dplyr")
+setwd("C:/Users/John/Desktop/workout01/data")
+source("../code/make-shots-data-script.r")
+
+
+court_file <- "../images/nba-court.jpg"
+
+court_image <- rasterGrob(
+  readJPEG(court_file),
+  width = unit(1, "npc"),
+  height = unit(1, "npc"))
+
+thompson_shot_chart <- ggplot(data = thompson) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Klay Thompson (2016 season)') +
+  theme_minimal()
+
+ggsave("../images/klay-thompson-shot-chart.pdf", width=6.5,height=5)
+
+iguodala_shot_chart <- ggplot(data = iguodala) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Andre Iguodala (2016 season)') +
+  theme_minimal()
+
+ggsave("../images/andre-iguodala-shot-chart.pdf", width=6.5,height=5)
+
+durant_shot_chart <- ggplot(data = durant) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Kevin Durant (2016 season)') +
+  theme_minimal()
+
+ggsave("../images/kevin-durant-shot-chart.pdf", width=6.5,height=5)
+
+curry_shot_chart <- ggplot(data = curry) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Stephen Curry (2016 season)') +
+  theme_minimal()
+
+ggsave("../images/stephen-curry-shot-chart.pdf", width=6.5,height=5)
+
+green_shot_chart <- ggplot(data = green) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) +
+  ggtitle('Shot Chart: Draymond Green (2016 season)') +
+  theme_minimal()
+
+ggsave("../images/draymond-green-shot-chart.pdf", width=6.5,height=5)
+
+gsw_shots <- rbind(curry,iguodala,durant,thompson,green)
+
+gsw_shot_charts <- ggplot(data=gsw_shots) +
+  annotation_custom(court_image, -250, 250, -50, 420) +
+  geom_point(aes(x = x, y = y, color = shot_made_flag)) +
+  ylim(-50, 420) + facet_wrap(~ name)
+  #ggtitle('Shot Chart: Draymond Green (2016 season)') +
+  theme_minimal()
+  
+ggsave("../images/gsw-shot-charts.pdf",width=8,height=7)
+
+
+
+
---
+++ workout1/code/make-shots-data-script.R
@@ -0,0 +1,71 @@
+
+##Are they supposed to be commented?
+#• title: short title
+#description: a short description of what the script is about
+#input(s): what are the inputs required by the script?
+#output(s): what are the outputs created when running the script?
+setwd("C:/Users/John/Desktop/workout01/data")
+
+curry <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE)
+iguodala <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE)
+green <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE)
+durant <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE)
+thompson <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE)
+
+curry$name <- "Stephen Curry"
+iguodala$name <- "Andre Iguodala"
+green$name <- "Draymond Green"
+durant$name <- "Kevin Durant"
+thompson$name <- "Klay Thompson"
+
+curry$shot_made_flag[curry$shot_made_flag=="y"] <- "shot_yes"
+curry$shot_made_flag[curry$shot_made_flag=="n"] <- "shot_no"
+iguodala$shot_made_flag[iguodala$shot_made_flag=="y"] <- "shot_yes"
+iguodala$shot_made_flag[iguodala$shot_made_flag=="n"] <- "shot_no"
+green$shot_made_flag[green$shot_made_flag=="y"] <- "shot_yes"
+green$shot_made_flag[green$shot_made_flag=="n"] <- "shot_no"
+durant$shot_made_flag[durant$shot_made_flag=="y"] <- "shot_yes"
+durant$shot_made_flag[durant$shot_made_flag=="n"] <- "shot_no"
+thompson$shot_made_flag[thompson$shot_made_flag=="y"] <- "shot_yes"
+thompson$shot_made_flag[thompson$shot_made_flag=="n"] <- "shot_no"
+
+curry$minute <- curry$period*12 - curry$minutes_remaining
+iguodala$minute <- iguodala$period*12 - iguodala$minutes_remaining
+green$minute <- green$period*12 - green$minutes_remaining
+durant$minute <- durant$period*12 - durant$minutes_remaining
+thompson$minute <- thompson$period*12 - thompson$minutes_remaining
+
+sink_name <- summary(iguodala) #using this because summary in sink is broken. (I think this is a known issue in R)
+sink(file="../output/andre-iguodala-summary.txt")
+print(sink_name)
+sink()
+
+sink_name <- summary(curry) #using this because summary in sink is broken. (I think this is a known issue in R)
+sink(file="../output/stephen-curry-summary.txt")
+print(sink_name)
+sink()
+
+sink_name <- summary(green) #using this because summary in sink is broken. (I think this is a known issue in R)
+sink(file="../output/draymond-green-summary.txt")
+print(sink_name)
+sink()
+
+sink_name <- summary(durant) #using this because summary in sink is broken. (I think this is a known issue in R)
+sink(file="../output/kevin-durant-summary.txt")
+print(sink_name)
+sink()
+
+sink_name <- summary(thompson) #using this because summary in sink is broken. (I think this is a known issue in R)
+sink(file="../output/klay-thompson-summary.txt")
+print(sink_name)
+sink()
+
+shots_data <- rbind(summary(green),summary(curry),summary(thompson),summary(iguodala),summary(durant))
+
+write.csv(shots_data,file="../output/shots_data.csv")
+
+sink_name <- summary(shots_data)
+sink("../output/shots-data-summary.txt")
+print(sink_name)
+sink()
+
